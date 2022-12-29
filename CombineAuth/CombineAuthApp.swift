7f540340b254d8092e8fa3dfd36d9270faff039c
Darya Zhitova <lilleeash@icloud.com>
2022-12-29 15:29:29 +0300

    Initial Commit

---
+++ CombineAuth/CombineAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAuthApp.swift
+//  CombineAuth
+//
+//  Created by Darya Zhitova on 29.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
